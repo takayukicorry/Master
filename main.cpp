306537997aebaeb5f836d9ed5f43bb4669aa4990
takayukicorry <takamas08050729@gmail.com>
2017-03-11 15:43:14 +0900

    backup

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Master
+//
+//  Created by 増田貴行 on 2017/03/10.
+//  Copyright © 2017年 増田貴行. All rights reserved.
+//
+
+#include <iostream>
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+#include <BulletDynamics/btBulletDynamicsCommon.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
