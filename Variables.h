870d5b11d225081c64f14324c19aa27c10858358
takayukicorry <takamas08050729@gmail.com>
2018-01-05 12:31:01 +0900

    NEAT start

--- Variables.h
+++ Variables.h
@@ -67,7 +67,7 @@ enum CollisionGroup{
 #define RADIUS_TF 1
 #define INIT_POS_Y LENGTH + RADIUS_TF*3 + 2
 #define NUM_GROUND 3
-#define NUM_TF_UNIT 4*3//>=4
+#define NUM_TF_UNIT 12//>=4
 #define NUM_TF NUM_TF_UNIT*NUM_JOINT
 #define DL_TIME 120
 #define RE_TIME 360
